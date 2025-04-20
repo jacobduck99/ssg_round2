4f5e52833a2a74d5b9b611fda4bd860b5e6614f8
jacobduck99 <jacob.formaz@gmail.com>
2025-04-20 16:46:19 +1000

    Changed link in html

--- src/copy_static.py
+++ src/copy_static.py
@@ -2,8 +2,6 @@ import os, os.path
 import shutil
 
 
-
-
 def copy_files_recursive(source_dir_path, dest_dir_path):
     if not os.path.exists(dest_dir_path):
         os.makedirs(dest_dir_path, exist_ok=True)
